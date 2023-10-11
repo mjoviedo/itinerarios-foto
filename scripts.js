a544aed13941f6274d4e10c6a77a7bb312feb79e
mjoviedo <128503807+mjoviedo@users.noreply.github.com>
2023-10-11 11:01:19 -0300

    pruebas revelado

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+// REVELADO 1
+
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.remove("mystyle");
+  } 
+
+
+
+// REVELADO 2
+
+  function FadeOut() {
+    const tapa = document.getElementById('tapa1');
+    
+    tapa.style.opacity = '0';
+ }
+
+
+ // REVELADO 3
+
+ function Fade() {
+    document.getElementById("revelado").classList = 'animate';
+ }
